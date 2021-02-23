2f3b5f8c0a4332a31e012ed68af135a1d697680f
Valerii Korobov <v.korobov@mangotele.com>
2021-02-24 02:05:51 +0300

    Added methods for work with webchats

--- example/main.py
+++ example/main.py
@@ -3,7 +3,6 @@ from os import environ
 from MangoBot import MangoBot
 
 environ.setdefault('login', 'LOGIN')
-
 environ.setdefault('password', 'PASSWORD')
 
 
@@ -13,14 +12,42 @@ def test(self, event):
 
 
 def test2(self, event):
-    self.sendSms(event.message.payload.body, 'Test SMS')
+    self.sendSms(event.message.payload.body, f'Test SMS - {event.message.account}')
+
+
+def test3(self, event):
+    text = f'''Event: SMS delivery status
+
+User: {self.findUserById(event.message.sender)}
+To number: {event.message.payload.number}
+
+Status: {event.message.payload.status}
+
+Text: {event.message.payload.body}'''
+    self.sendBasicText(chat_id="mango@telecom.favorite.service", text=text)
+
+
+def test4(self, event):
+    text = f'''Event: SMS delivery status
+
+User: {self.findUserById(event.fromUser)}
+To number: {event.message.payload.number}
+
+Status: {event.message.payload.status}
+
+Text: {event.message.payload.body}'''
+    self.sendBasicText(chat_id="mango@telecom.favorite.service", text=text)
 
 
 t = MangoBot(login=environ.get('login'), password=environ.get('password'))
 
 for _ in range(0, 5):
-    t.addEventHandler(str(uuid4()), test)
-    t.addEventHandler(r'\d{11}', test2)
+    t.addEventHandler('401', str(uuid4()), test)
+    t.addEventHandler('401', r'\d{11}', test2)
+    t.addEventHandler('808', r'\d{11}', test2)
+
+t.addEventHandler('401', '', test3)
+t.addEventHandler('402', '', test4)
 
 if __name__ == "__main__":
     try:
