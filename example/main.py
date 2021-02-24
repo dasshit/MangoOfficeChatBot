92b6953c166a9724176d661f95ba6b667261ff6f
Valerii Korobov <v.korobov@mangotele.com>
2021-02-24 03:52:03 +0300

    Added comments

--- example/main.py
+++ example/main.py
@@ -2,20 +2,20 @@ from uuid import uuid4
 from os import environ
 from MangoBot import MangoBot
 
-environ.setdefault('login', 'LOGIN')
-environ.setdefault('password', 'PASSWORD')
+environ.setdefault('login', 'LOGIN')  # Здесь указываем логин
+environ.setdefault('password', 'PASSWORD')  # Пароль
 
 
-def test(self, event):
+def test(self, event):  # Тестовая функция для обработки входящих сообщений (Эхо бот)
     self.logger.info(event)
     self.factoryBasicText(event.message.account, '{}-{}'.format(event.message.payload.body, uuid4()))
 
 
-def test2(self, event):
+def test2(self, event):  # Отправка СМС
     self.sendSms(event.message.payload.body, f'Test SMS - {event.message.account}')
 
 
-def test3(self, event):
+def test3(self, event):  # Обработка события о статусе СМС и отправка сообщения в Избранное
     text = f'''Event: SMS delivery status
 
 User: {self.findUserById(event.message.sender)}
@@ -27,7 +27,7 @@ Text: {event.message.payload.body}'''
     self.sendBasicText(chat_id="mango@telecom.favorite.service", text=text)
 
 
-def test4(self, event):
+def test4(self, event):  # Обработка события о статусе СМС и отправка сообщения в Избранное
     text = f'''Event: SMS delivery status
 
 User: {self.findUserById(event.fromUser)}
@@ -39,19 +39,19 @@ Text: {event.message.payload.body}'''
     self.sendBasicText(chat_id="mango@telecom.favorite.service", text=text)
 
 
-t = MangoBot(login=environ.get('login'), password=environ.get('password'))
+bot = MangoBot(login=environ.get('login'), password=environ.get('password'))  # Создаем экземпляр бота
 
-for _ in range(0, 5):
-    t.addEventHandler('401', str(uuid4()), test)
-    t.addEventHandler('401', r'\d{11}', test2)
-    t.addEventHandler('808', r'\d{11}', test2)
+for _ in range(0, 5):  # Добавляем созданные функции как обработчики
+    bot.addEventHandler('401', str(uuid4()), test)
+    bot.addEventHandler('401', r'\d{11}', test2)
+    bot.addEventHandler('808', r'\d{11}', test2)
 
-t.addEventHandler('401', '', test3)
-t.addEventHandler('402', '', test4)
+bot.addEventHandler('401', '', test3)
+bot.addEventHandler('402', '', test4)
 
 if __name__ == "__main__":
     try:
-        t.execute()
-        t.endlessCycle()
+        bot.execute()
+        bot.endlessCycle()
     except Exception as error:
-        t.logger.exception(error)
+        bot.logger.exception(error)
